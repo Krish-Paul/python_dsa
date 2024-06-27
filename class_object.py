80fae716909651025e6d978268ac81308b8d6f4a
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-06-27 10:16:43 +0530

    Update class_object.py

--- class_object.py
+++ class_object.py
@@ -1,21 +1,26 @@
-class Test:
-def
-_init_
-_(self,a, b):
-self.a=a
-self.b=b
-def
-show (self):
-print(self.a,self.b)
-@staticmethod
-def
-f2() :
-pass
-@classmethod
-def
-f3(cls):
-pass
-t1=Test (3,4)
-t2=Test (5,6)
-t1. show()
-t2. show()
+  class Test:
+    x=5
+  def
+  _init_
+  _(self,a, b):
+  self.a=a
+  self.b=b
+  def
+  show (self):
+  print(self.a,self.b)
+  @staticmethod
+  def
+  f2() :
+    print("hello")
+  pass
+  @classmethod
+  def
+  f3(cls):
+  pass
+  t1=Test (3,4)
+  t2=Test (5,6)
+  t1. show()
+  t2. show()
+  Test.f3()
+  Test.f2()
+
