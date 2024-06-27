8bc2dfb7bb6f3d752a535cec178df203d4b2a9cf
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-06-27 10:03:04 +0530

    Create class_object.py

---
+++ class_object.py
@@ -0,0 +1,21 @@
+class Test:
+def
+_init_
+_(self,a, b):
+self.a=a
+self.b=b
+def
+show (self):
+print(self.a,self.b)
+@staticmethod
+def
+f2() :
+pass
+@classmethod
+def
+f3(cls):
+pass
+t1=Test (3,4)
+t2=Test (5,6)
+t1. show()
+t2. show()
