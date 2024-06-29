7344f3cbd3150d7dcf8f870953977b6b8febb71a
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-06-29 08:56:46 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -14,3 +14,11 @@ class Node:
     def __init__(self,item=None,next=None):
         self.item=item
         self.next=next
+class SLL:
+    def __init(self,start=None):
+        self.start=start
+    def is_empty(self):
+        return self.start==None
+    def insert_at_start(self,data):
+        n=Node(data,self.start)
+        self.start=n
