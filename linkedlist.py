c0b697ea285738521540760b468bb42a7efede23
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-01 10:20:12 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -17,7 +17,7 @@ class Node:
         self.item=item
         self.next=next
 class SLL:
-    def __init(self,start=None):
+    def __init__(self,start=None):
         self.start=start
     def is_empty(self):
         return self.start==None
@@ -27,7 +27,7 @@ class SLL:
     def insert_at_last(self,data):
         n=Node(data)
         if not self.is_empty():
-            temp=start
+            temp=self.start
             while temp.next is not None:
                 temp=temp.next
             temp.next=n
@@ -40,3 +40,53 @@ class SLL:
                 return temp
             temp=temp.next
         return None
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n=Node(data,temp.next)
+            temp.next=n
+    def printlist(self):
+        temp=self.start
+        while temp is not None:
+            print(temp.item)
+            temp=temp.next
+    def delete_first(self):
+        if self.start is not None:
+            self.start=self.start.next
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start=None
+        else:
+            temp=self.start
+            while temp.next.next is not None:
+                temp=temp.next
+            temp.next.next=None
+    def delete_item(self,data):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            if self.start.item==data:
+                self.start=None
+        else:
+            temp=self.start
+            if temp.item==data:
+                self.start=temp.next
+            else:
+                while temp.next is not None:
+                    if temp.next.item==data:
+                        temp.next=temp.next.next
+                        break
+                    temp=temp.next
+class SLLiterate:
+    def __init__(self,start):
+        self.current=start
+#driver code
+mylist=SLL()
+mylist.insert_at_start(20)
+mylist.insert_at_start(10)
+mylist.insert_at_last(30)
+mylist.insert_after(mylist.search(20),25)
+mylist.printlist()
+mylist.delete_item(30)
+print()
