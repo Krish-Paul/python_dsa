c6783f6e80f4323cdd97bed97898d06123916616
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-07-02 09:03:01 +0530

    Create doublylinkedlist.py

---
+++ doublylinkedlist.py
@@ -0,0 +1,9 @@
+#Define a class Node to describe a node of a doubly linked list.
+#Define a class DLL to implement Doubly Linked List with _init_() method to create and initialise start reference variable.
+#Define a method is_empty) to check if the linked list is empty in DLL class.
+#In class DLL, define a method insert _at_start) to insert an element at the starting of the list.
+#In class DLL, define a method insert _at_last) to insert an element at the end of the list.
+#In class DLL, define a method search) to find the node with specified element value
+#In class DLL, define a method insert _after) to insert a new node after a given node of the list.
+#In class DLL, define a method to print all the elements of the list.
+#In class DLL, implement iterator for DLL to access all the elements of the list in a sequence.
